f74b845ee02c18e80f20a574c285e4d37170037c
Alexander Tsybulsky <atsybulsky@sbcg.com.ua>
2020-01-08 19:23:13 +0200

    user re named groups

--- handler.js
+++ handler.js
@@ -122,14 +122,12 @@ function fetchResource(url) {
 }
 
 function parseSourceFile(fileData, attrName) {
-    const abapConstantRe = new RegExp(`constants\\s+${attrName}\\s+(type\\s+\\S+\\s+)?value\\s+(\\S+)(\\s|\\.)`, 'i');
+    const abapConstantRe = new RegExp(`constants\\s+${attrName}\\s+(type\\s+\\S+\\s+)?value\\s+(?<attrValue>\\S+)(\\s|\\.)`, 'i');
     const match = abapConstantRe.exec(fileData);
-    // console.log(abapConstantRe);
-    // console.log(match);
-    if (!match || !match[2]) { // TODO named groups
+    if (!match || !match.groups || !match.groups.attrValue) {
         throw Error('Could not find attr in the file');
     }
-    let version = match[2];
+    let version = match.groups.attrValue;
     if (/^'\S+'$/.test(version)) version = version.slice(1,-1);
     return version;
 }
