e9e3f1fa7e47727e30da4185475911b472663a49
Alexander Tsybulsky <atsybulsky@sbcg.com.ua>
2020-01-10 00:24:32 +0200

    remove queryStringParameters from localtest

--- localtest-const.js
+++ localtest-const.js
@@ -4,13 +4,6 @@ const handler = require('./handler');
 
 (async function main() {
     const event = {
-        queryStringParameters: {
-            type: 'github',
-            owner: 'sbcgua',
-            repo: 'mockup_loader',
-            file: 'src/zif_mockup_loader_constants.intf.abap',
-            attr: 'version',
-        },
         resource: '/get-abap-version-shield-json/{sourcePath}',
         path: '/get-abap-version-shield-json/github/sbcgua/mockup_loader/src/zif_mockup_loader_constants.intf.abap/version',
         pathParameters: {
