3d66b8c41260e319a050737ac5fe261f033f30bd
Alexander Tsybulsky <atsybulsky@sbcg.com.ua>
2020-02-22 11:34:41 +0200

    add local tests

---
+++ localtest-apack-deps.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const handler = require('./handler');
+
+(async function main() {
+    const event = {
+        // resource: '/version-shield-json/{sourcePath}',
+        // path: '/version-shield-json/github/SAP-samples/abap-platform-jak/.apack-manifest.xml',
+        pathParameters: {
+            sourcePath: 'github/mbtools/mbt-versions/.apack-manifest.xml/dependencies/github.com/mbtools/mbt-bc-cts-req'
+        },
+    };
+    const context = {
+
+    };
+
+    let result = await handler.getShieldJson(event, context);
+    if (result instanceof Promise) result = await result;
+    console.log(result);
+})();
