a96e4a25dd4c538dce9dc0a5fc9ffc9a3e0d57ce
Alexander Tsybulsky <atsybulsky@sbcg.com.ua>
2025-08-13 07:10:45 +0300

    remove domain command from shell scripts

--- bin/deploy-prod.sh
+++ bin/deploy-prod.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 STAGE=prod
 
-echo "Creating $STAGE domain ..."
-serverless create_domain --stage $STAGE
+# echo "Creating $STAGE domain ..."
+# serverless create_domain --stage $STAGE
 
 echo "Deploying $STAGE lambda ..."
 serverless deploy --stage $STAGE
